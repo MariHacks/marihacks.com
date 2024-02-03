d53926a0dadf801d31c1249833660e29bfbcf222
JStephenHuang <jstephhuang@gmail.com>
2024-02-03 00:55:54 -0500

    text

--- src/docs/faq.ts
+++ src/docs/faq.ts
@@ -10,8 +10,8 @@ export const faqColOne: faqCol = [
     question: "What is a hackathon?",
     answer:
       "A hackathon is where you turn your crazy ideas into real stuff." +
-      "It is an event where beginner and experienced hackers gather together to work on innovative solutions to real-world problems." +
-      "At MariHacks, you can look forward to amazing guest speakers, helpful workshops, skilled mentors and, of course, awesome hacking!",
+      " It is an event where beginner and experienced hackers gather together to work on innovative solutions to real-world problems." +
+      " At MariHacks, you can look forward to amazing guest speakers, helpful workshops, skilled mentors and, of course, awesome hacking!",
   },
   {
     index: 2,
@@ -26,7 +26,7 @@ export const faqColOne: faqCol = [
     question: "What if I don't know how to code?",
     answer:
       "Then you've come to the right place! " +
-      "We will have great workshops for beginners as well as experienced mentors to get your started on your coding journey.",
+      " We will have great workshops for beginners as well as experienced mentors to get your started on your coding journey.",
   },
   {
     index: 4,
@@ -42,7 +42,7 @@ export const faqColTwo: faqCol = [
     question: "How many hackers per team?",
     answer:
       "Teams have a maximum of 4 members. " +
-      "You can select your teammates when you register or you can register individually without specified teammates.",
+      " You can select your teammates when you register or you can register individually without specified teammates.",
   },
 
   {
@@ -66,7 +66,7 @@ export const faqColTwo: faqCol = [
     question: "When will applications open?",
     answer:
       "Hacker applications will open on February 5th." +
-      "Keep an eye out for the application form in our application center!",
+      " Keep an eye out for the application form in our application center!",
   },
   {
     index: 13,
@@ -74,8 +74,8 @@ export const faqColTwo: faqCol = [
     question: "Who can stay overnight at MariHacks?",
     answer:
       "CEGEP students, volunteers and mentors can stay overnight at MariHacks as long as all necessary information is provided (see the registration forms).s" +
-      "Judges and guest speakers are welcome to stay as well." +
-      "However, if you are a high school student, you will not be able to stay overnight.",
+      " Judges and guest speakers are welcome to stay as well." +
+      " However, if you are a high school student, you will not be able to stay overnight.",
   },
 ];
 export const faqColThree: faqCol = [
@@ -85,7 +85,7 @@ export const faqColThree: faqCol = [
     question: "I'm interested in judging!",
     answer:
       "If you have professional experience related to MariHacks, we'd love to invite you. " +
-      "Judge registration will open soon, please stay tuned or send an application through application center!",
+      " Judge registration will open soon, please stay tuned or send an application through application center!",
   },
   {
     index: 10,
@@ -93,7 +93,7 @@ export const faqColThree: faqCol = [
     question: "I'm interested in presenting a workshop!",
     answer:
       "We'd love to have you present a workshop on any topic related to MariHacks." +
-      "The panelist registration form will be open soon, feel free to send an application through application center!",
+      " The panelist registration form will be open soon, feel free to send an application through application center!",
   },
   {
     index: 11,
@@ -101,7 +101,7 @@ export const faqColThree: faqCol = [
     question: "I'm interested in mentoring!",
     answer:
       "If you are an experienced programmer who loves to help others, we'd love to have you too! " +
-      "The mentor registration form will be posted soon, or you can also send an application through the application center!",
+      " The mentor registration form will be posted soon, or you can also send an application through the application center!",
   },
 
   {
