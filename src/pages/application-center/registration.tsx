37b36cd90703303174aa788b0c77575f7c7604e0
KineticJetIce245 <kineticjetice245@gmail.com>
2025-03-10 22:59:22 -0400

    update the reg

--- src/pages/application-center/registration.tsx
+++ src/pages/application-center/registration.tsx
@@ -1,46 +1,48 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import BackArrow from "../../components/BackArrow";
 import { imageManager } from "../../helper/ressourceManagers";
 
 const Registration = () => {
-  // const mariHacksEventDate = new Date("4/5/2024");
+  // monthIndex = month - 1
+  const mariHacksEventDate = new Date(2025, 2, 24, 23, 59, 59);
 
-  // const countdown = () => {
-  //   const currentDate = Date.now();
+  const countdown = () => {
+    const currentDate = Date.now();
 
-  //   const totalSecond = (mariHacksEventDate.getTime() - currentDate) / 1000;
+    const totalSecond = (mariHacksEventDate.getTime() - currentDate) / 1000;
 
-  //   setDays(formatTime(Math.floor(totalSecond / 3600 / 24)));
-  //   setHours(Math.floor(totalSecond / 3600) % 24);
-  //   setMinutes(Math.floor(totalSecond / 60) % 60);
-  //   setSeconds(Math.floor(totalSecond % 60));
-  // };
+    setDays(formatTime(Math.floor(totalSecond / 3600 / 24)));
+    setHours(Math.floor(totalSecond / 3600) % 24);
+    setMinutes(Math.floor(totalSecond / 60) % 60);
+    setSeconds(Math.floor(totalSecond % 60));
+  };
 
-  // const [days, setDays] = useState<number>(0);
-  // const [hours, setHours] = useState<number>(0);
-  // const [minutes, setMinutes] = useState<number>(0);
-  // const [seconds, setSeconds] = useState<number>(0);
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
 
-  // const formatTime = (time: number): number => {
-  //   return time < 10 ? Number(`0${time}`) : time;
-  // };
+  const formatTime = (time: number): number => {
+    return time < 10 ? Number(`0${time}`) : time;
+  };
 
-  // useEffect(() => {
-  //   countdown();
-  // }, []);
+  useEffect(() => {
+    countdown();
+  }, []);
 
-  // setInterval(() => {
-  //   if (seconds !== 0) {
-  //     countdown();
-  //   }
-  // }, 1000);
+  setInterval(() => {
+    if (seconds !== 0) {
+      countdown();
+    }
+  }, 1000);
 
   return (
     <div className="acp-container">
       <BackArrow text="Home" link="/" />
       <div className="acp-card-bg mx-auto lg:w-1/2 flex flex-col items-center">
         <div className="flex flex-col items-center text-center">
+          {/*
           <p className="font-bold acp-title">Register</p>
           <p className="font-bold">Welcome to MariHacks VIII!</p>
           <p className="my-4">
@@ -51,26 +53,21 @@ const Registration = () => {
             Don't forget to follow us on Instagram (@marihacks)! You can also
             reach out to our team through team@marihacks.com.
           </p>
-          {/* <p className="font-bold">Welcome to MariHacks VII!</p>
+          */}
+          <p className="font-bold">Welcome to MariHacks VIII!</p>
           <p className="my-4">
             We're thrilled to invite all Cegep and high school students to join
-            us for an unforgettable hackathon experience on{" "}
-            <span className="font-bold">April 5-6, 2024</span>. MariHacks is an
+            us for a memorable hackathon experience on{" "}
+            <span className="font-bold">April 4-5<sup>th</sup>, 2025</span>. MariHacks is an
             opportunity for young minds to come together, push boundaries, and
             turn their creative ideas into reality!
-          </p>
-          <p>
-            Applications for MariHacks VII are{" "}
-            <span className="font-bold">is now closed.</span> We will send out
-            application decisions soon!
-          </p> */}
-          {/* 
+          </p> 
           <div className="flex flex-col items-center mt-8">
             <p className="">Time left before the event:</p>
             <p>
               {days} days {hours} hrs {minutes} min {seconds} sec
             </p>
-          </div> */}
+          </div>
           <a
             // to disable style: acp-btn-disabled-styles
             // to reable clicking: acp-btn-disabled-position
