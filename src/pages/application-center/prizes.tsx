ee1977ee69bf0ab840fe67de95275b744982d131
KineticJetIce245 <kineticjetice245@gmail.com>
2025-03-13 22:51:09 -0400

    commenting unused comp

--- src/pages/application-center/prizes.tsx
+++ src/pages/application-center/prizes.tsx
@@ -2,6 +2,8 @@ import BackArrow from "../../components/BackArrow";
 import { imageManager } from "../../helper/ressourceManagers";
 
 const Prizes = () => {
+
+  /*
   const bulletPoints = [
     "Advanced category 1st Place Prize: 125$ Amazon Gift card",
     "Advanced category 2nd Place Prize: 100$ Amazon Gift card",
@@ -10,6 +12,7 @@ const Prizes = () => {
     "Beginner category 3rd Place Prize: 50$ Amazon Gift card",
     "Beginner category 3rd Place Prize: 25$ Amazon Gift card",
   ];
+  */
   return (
     <div className="acp-container">
       <BackArrow text="Application center" link="/application-center" />
@@ -18,9 +21,9 @@ const Prizes = () => {
           <p className="font-bold acp-title">Prizes</p>
           <p className="">
             MariHacks offers lots of great prizes to add to the fun! The prizes
-	    for MariHacks VIII are to be determinated!
-          </p>	
-	  {/*
+            for MariHacks VIII are to be determinated!
+          </p>  
+    {/*
           <p className="">
             MariHacks offers lots of great prizes to add to the fun! During
             MariHacks VIII, 11,000 $ CAD worth of prizes were offered to our
@@ -33,9 +36,9 @@ const Prizes = () => {
             </a>{" "}
             for more details). Stay tuned for our prize reveal this year!
           </p>
-	  */}
+    */}
           <br className="my-2" />
-	  {/*
+    {/*
           <p>Little sneakpeak:</p>
           <ul className="list-disc ml-5">
             {bulletPoints.map((bulletPoint, key) => {
@@ -46,7 +49,7 @@ const Prizes = () => {
               );
             })}
           </ul>
-	  */}
+    */}
 
           <a
             className="acp-apply-btn acp-btn-disabled-styles acp-btn-disabled-position"
@@ -55,13 +58,13 @@ const Prizes = () => {
             See more
           </a>
         </div>
-	{/*
+  {/*
         <img
           className="acp-img"
           src={imageManager("prizes").acpImg}
           alt="photo"
         />
-	  */}
+    */}
       </div>
     </div>
   );
